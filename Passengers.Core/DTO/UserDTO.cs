aa411b42b0791abfe94923e5523203ffd3f038c4
Szymon Ratajczak <daniel.kruk1@interia.pl>
2017-10-11 20:44:28 +0200

    Update UserDTO.cs

--- Passengers.Core/DTO/UserDTO.cs
+++ Passengers.Core/DTO/UserDTO.cs
@@ -5,8 +5,10 @@ using System.Text;
 namespace Passengers.Core.DTO
 {
     //this class stores  flat object' s user 
-    //I want to have only flat object to have no metohds which can disturbs my API 
-    //flat objet can be send where i want i can do wtih it whatever  i want
+    //I want to have only flat object to have no metohds which can disturbs my API
+    //flat object are very useful for safety reasons- i don't want to return full information 
+    //about object e.g i don't want return methods and so on  
+    //flat objet can be send where i want and i can do with it whatever  i want
 public    class UserDTO
     {
         public Guid ID { get;  set; }
